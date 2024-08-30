938b8825cced720b2040b8c20799ea93e89368a9
mariussde <160734137+mariussde@users.noreply.github.com>
2024-08-30 20:47:59 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/config/portfolio.config.ts
@@ -0,0 +1,14 @@
+import type { Portfolio } from "@/types";
+
+export const portfolioConfig: Portfolio = {
+  name: "Rudro Dip Sarker",
+  tagline: "I gaslight rocks 🪨 to think 🧠",
+  resume: "https://drive.google.com/file/d/1IZrep-b2l6rbyY3cgvJjbrI2Qa8kmugN/view?usp=drive_link",
+  links: {
+    github: "https://github.com/rudrodip",
+    twitter: "https://x.com/rds_agi",
+    mail: "mailto:official.rudrodipsarker@gmail.com",
+    discord: "https://discordapp.com/users/841126921886498817",
+    linkedin: "https://linkedin.com/in/rudrodip",
+  }
+}

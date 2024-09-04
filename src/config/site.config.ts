dc20bed9f96edb44a42e6fc518eba9ee713ccdc2
Marius Lefter <160734137+mariussde@users.noreply.github.com>
2024-09-04 12:20:20 +0200

    pequeño cambio bio

--- src/config/site.config.ts
+++ src/config/site.config.ts
@@ -4,7 +4,7 @@ export const siteConfig: Site = {
   name: "mariuslefter.com",
   title: "Marius Lefter | mariuslefter.com | Building a better future",
   description:
-    "Hey 👋 Marius here! I am a software engineer. I build web apps, mobile applications, backend servers, ML solutions and much more.",
+    "Hey 👋 Marius here! I am a software engineer based in Spain. I build full stack web apps, mobile applications, backend servers, ML solutions and much more.",
   keywords: [
     "Marius Lefter",
     "mariuslefter.com",
